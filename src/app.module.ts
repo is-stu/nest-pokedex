9d3ffac0831918695deb2f1fed8fa5885cc1729d
Stewar Marin <stewar1020@gmail.com>
2024-09-09 15:06:52 -0500

    feat: Creating res of pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../public'),
+      exclude: ['/api*'],
+      renderPath: '/',
     }),
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
