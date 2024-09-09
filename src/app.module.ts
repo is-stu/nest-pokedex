cef177bcfaf53b8a95fcae9a040e7bc81714e060
Stewar Marin <stewar1020@gmail.com>
2024-09-09 15:58:34 -0500

    feat: Create pokemon entity and db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -10,6 +11,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
       exclude: ['/api*'],
       renderPath: '/',
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokedex'),
     PokemonModule,
   ],
   controllers: [],
