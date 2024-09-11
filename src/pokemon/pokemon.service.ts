ae3895cf15bcbaf867ccaadbb7c0a00ac5d4bcb2
Stewar Marin <stewar1020@gmail.com>
2024-09-10 21:03:42 -0500

    feat: FindOne by term or id

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -2,10 +2,11 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -37,8 +38,35 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(id: string) {
+    let pokemon: Pokemon;
+
+    // Verification by NO in the database
+    if (!isNaN(+id)) {
+      pokemon = await this.pokemonModel.findOne({
+        no: id,
+      });
+    }
+
+    // Verification by mongoId in the database
+    if (!pokemon && isValidObjectId(id)) {
+      pokemon = await this.pokemonModel.findById(id);
+    }
+
+    // Verification by name in the database
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({
+        name: id.toLowerCase(),
+      });
+    }
+
+    if (!pokemon) {
+      throw new NotFoundException(
+        `Cannot find pokemon by the term or id '${id}'`,
+      );
+    }
+
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
