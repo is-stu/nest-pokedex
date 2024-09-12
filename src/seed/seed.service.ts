556bbee03e91f072b279c66bfc67689551e788dc
Stewar Marin <stewar1020@gmail.com>
2024-09-11 21:53:57 -0500

    feat: Export and import pokemon service into seed MO

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @Inject(PokemonService)
+    private readonly pokemonService: PokemonService,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({ name, no });
+      const pokemonToInsert: CreatePokemonDto = {
+        name,
+        no,
+      };
+      await this.pokemonService.create(pokemonToInsert);
     });
     return data.results;
   }
